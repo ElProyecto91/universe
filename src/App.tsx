02023d6500ad231124688ac06532b032b0a3ce5c
ElProyecto91 <esxdinero@gmail.com>
2026-08-31 00:13:54 +0100

    Actualizar App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
 import Onboarding from './pages/Onboarding'
+import Universo from './pages/Universo'
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/onboarding" element={<Onboarding />} />
+        <Route path="/universo" element={<Universo />} />
       </Routes>
     </BrowserRouter>
   )
