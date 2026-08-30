30998403e5e7cbfa5ee0fe4b5af8a4304a732e67
ElProyecto91 <esxdinero@gmail.com>
2026-08-30 23:56:22 +0100

    Actualizar App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Landing from './pages/Landing'
+import Onboarding from './pages/Onboarding'
+
 export default function App() {
   return (
-    <div style={{ color: 'white', background: 'black', minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <h1>UNIVERSE funciona</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/onboarding" element={<Onboarding />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
