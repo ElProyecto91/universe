23ee6e2437415de907808599f6e0c55453186466
ElProyecto91 <esxdinero@gmail.com>
2026-08-31 02:03:26 +0100

    Actualizar App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,12 @@ import Onboarding from './pages/Onboarding'
 import Universo from './pages/Universo'
 import Guia from './pages/Guia'
 import Tarot from './pages/Tarot'
+import TarotDiario from './pages/TarotDiario'
+import IChing from './pages/IChing'
+import Omikuji from './pages/Omikuji'
+import BaZi from './pages/BaZi'
+import Numerologia from './pages/Numerologia'
+import Tradiciones from './pages/Tradiciones'
 
 export default function App() {
   return (
@@ -14,6 +20,12 @@ export default function App() {
         <Route path="/universo" element={<Universo />} />
         <Route path="/guia" element={<Guia />} />
         <Route path="/tarot" element={<Tarot />} />
+        <Route path="/tarot-diario" element={<TarotDiario />} />
+        <Route path="/iching" element={<IChing />} />
+        <Route path="/omikuji" element={<Omikuji />} />
+        <Route path="/bazi" element={<BaZi />} />
+        <Route path="/numerologia" element={<Numerologia />} />
+        <Route path="/tradiciones" element={<Tradiciones />} />
       </Routes>
     </BrowserRouter>
   )
