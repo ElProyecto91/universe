bef93cb68cb9067f77d61b05e2616f58311672e8
ElProyecto91 <esxdinero@gmail.com>
2026-09-05 10:34:17 +0100

    Actualizar main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import ErrorBoundary from './components/ErrorBoundary'
 
 const root = document.getElementById('root')
 
 if (root) {
   ReactDOM.createRoot(root).render(
     <React.StrictMode>
-      <App />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
     </React.StrictMode>
   )
 } else {
