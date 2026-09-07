be1079bf07e6d939d5f62a418de0807948e4d1f9
ElProyecto91 <esxdinero@gmail.com>
2026-09-08 00:37:08 +0100

    Actualizar tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,16 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      opacity: {
+        '92': '0.92',
+        '88': '0.88',
+        '85': '0.85',
+      },
+      backgroundColor: {
+        'page': '#080010',
+      },
+    },
+  },
   plugins: [],
 }
