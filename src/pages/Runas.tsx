d3cbb1619e375fcba33e20a2158b1288d8daf4e2
ElProyecto91 <esxdinero@gmail.com>
2026-09-15 00:24:13 +0100

    Actualizar Runas.tsx

--- src/pages/Runas.tsx
+++ src/pages/Runas.tsx
@@ -131,7 +131,7 @@ export default function Runas() {
       userId,
       usarLite: false,
       cacheable: false,
-      maxTokens: 2800,
+      maxTokens: 3200,
     })
 
     if (!result.error && result.texto) {
