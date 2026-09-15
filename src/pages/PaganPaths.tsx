1cd37d433cd8bcffb1dda9983e7b37094fad903f
ElProyecto91 <esxdinero@gmail.com>
2026-09-15 15:19:40 +0100

    Actualizar PaganPaths.tsx

--- src/pages/PaganPaths.tsx
+++ src/pages/PaganPaths.tsx
@@ -152,7 +152,7 @@ export default function PaganPaths() {
       userId,
       usarLite: false,
       cacheable: false,
-      maxTokens: 1800,
+      maxTokens: 2800,
     })
 
     if (result.error) {
