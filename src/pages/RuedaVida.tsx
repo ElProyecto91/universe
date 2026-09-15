8c3c633ec1c6cb9a3c53028dc9e62e6f553a4e9c
ElProyecto91 <esxdinero@gmail.com>
2026-09-15 13:02:27 +0100

    Actualizar RuedaVida.tsx

--- src/pages/RuedaVida.tsx
+++ src/pages/RuedaVida.tsx
@@ -75,7 +75,7 @@ export default function RuedaVida() {
       userId,
       usarLite: false,
       cacheable: false,
-      maxTokens: 2500,
+      maxTokens: 2800,
     })
 
     if (result.error) {
